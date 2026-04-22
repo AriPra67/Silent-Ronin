f2632adef7c13460fe3598e1889db7e3f08d6525
AriPra67 <ynx3297@autuni.ac.nz>
2026-04-22 15:43:20 +1200

    Fixed Player Movement and jump (using input manager)

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -13,13 +13,6 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Jumping")]
     public float jumpPower = 10f;
-    public int maxJumps = 2;
-    private int jumpsRemaining;
-
-    [Header("GroundCheck")]
-    public Transform groundCheckPos;
-    public Vector2 groundCheckSize = new Vector2(0.49f, 0.03f);
-    public LayerMask groundLayer;
 
     [Header("Gravity")]
     public float baseGravity = 2f;
@@ -28,28 +21,25 @@ public class PlayerMovement : MonoBehaviour
 
     void Awake()
     {
-        // Automatically assign Rigidbody2D if not assigned in Inspector
         if (rb == null)
             rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        // Horizontal movement
+        // Movement
         rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);
 
-        // Falling gravity
+        // Gravity
         if (rb.linearVelocity.y < 0)
         {
-            rb.gravityScale = baseGravity * fallGravityMult; // fall faster
+            rb.gravityScale = baseGravity * fallGravityMult;
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, Mathf.Max(rb.linearVelocity.y, -maxFallSpeed));
         }
         else
         {
             rb.gravityScale = baseGravity;
         }
-
-        GroundCheck();
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -59,36 +49,9 @@ public class PlayerMovement : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if (jumpsRemaining > 0)
+        if (context.performed)
         {
-            if (context.performed)
-            {
-                // Full jump
-                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
-                jumpsRemaining--;
-            }
-            else if (context.canceled && rb.linearVelocity.y > 0)
-            {
-                // Short jump
-                rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
-                jumpsRemaining--;
-            }
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
         }
-    }
-
-    private void GroundCheck()
-    {
-        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
-        {
-            jumpsRemaining = maxJumps;
-        }
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        // Visualize ground check area
-        Gizmos.color = Color.white;
-        Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
-    
     }
 }
