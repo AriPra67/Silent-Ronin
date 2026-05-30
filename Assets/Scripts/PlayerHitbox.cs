06dc06b5de239468d0b54ab1d42e246e68d8a0e6
Lin710862 <lin710862@gmail.com>
2026-05-30 22:24:19 +1200

    WARNING ERROR Test Fix with Finished Code
    WARNING Code contains compile errors unrelated to my features but I am unable to test features bc of this. DON'T push the code or merge it, this is for looking at if anyone needs it. Updated to current main branch. My features were added using prefabs this time rather than file changes.

--- Assets/Scripts/PlayerHitbox.cs
+++ Assets/Scripts/PlayerHitbox.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerHitbox : MonoBehaviour
 {
     public int damage = 1;
-    public bool attackMode = true; // can stay true for testing
+    public bool attackMode = true;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -11,12 +11,20 @@ public class PlayerHitbox : MonoBehaviour
 
         if (!attackMode) return;
 
-        EnemyHealth enemy = other.GetComponentInParent<EnemyHealth>();
-
+        EnemyHealth enemy = other.transform.GetComponentInParent<EnemyHealth>();
         if (enemy != null)
         {
             Debug.Log("ENEMY HIT!");
             enemy.TakeDamage(damage);
+            return;
+        }
+
+        PuzzleGiveHealth puzzle = other.transform.GetComponentInParent<PuzzleGiveHealth>();
+        if (puzzle != null)
+        {
+            Debug.Log("PUZZLE OBJECT HIT!");
+            puzzle.TakeDamage(damage);
+            return;
         }
     }
 
